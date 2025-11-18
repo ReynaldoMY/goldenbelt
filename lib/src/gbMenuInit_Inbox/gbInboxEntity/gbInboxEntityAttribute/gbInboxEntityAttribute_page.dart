7c1b2cdeaf911dad9fadc9f1a4b16d07b5b80273
Reynaldo Mamani <rmamani@pronetwork.com.pe>
2025-11-17 23:37:26 -0500

    Agregando botónes de visualización de documentos/políticas en visualización de entidades en la sección de inbox

--- lib/src/gbMenuInit_Inbox/gbInboxEntity/gbInboxEntityAttribute/gbInboxEntityAttribute_page.dart
+++ lib/src/gbMenuInit_Inbox/gbInboxEntity/gbInboxEntityAttribute/gbInboxEntityAttribute_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:goldenbelt/src/_models/gb_tentity_attribute_model.dart';
 import 'package:goldenbelt/src/_providers/gb_tentity_attribute_provider.dart';
+import 'package:goldenbelt/src/_providers/gb_tentity_provider.dart';
 import 'package:goldenbelt/src/gbMenuInit_Inbox/gbInboxEntity/gbInboxEntityAttribute/gbInboxEntityAttributeView/gbInboxEntityAttributeView_page.dart';
 import 'package:goldenbelt/src/gbMenuInit_Inbox/gbInboxEntity/gbInboxEntityAttribute/gbInboxEntityApproval/gbInboxEntityApproval_page.dart';
+import 'package:goldenbelt/src/gbMenuInit_Person/gbEntity/gbEntityAttribute/gbEntityPdfView/gbEntityPdfView_page.dart';
+import 'package:goldenbelt/src/gbMenuInit_Person/gbEntity/gbEntityAttribute/gbDocumentDownload/gbDocumentDownload_page.dart';
 
 import 'package:settings_ui/settings_ui.dart';
 import 'package:goldenbelt/src/_env/gbLanguage.dart';
@@ -35,8 +38,10 @@ class gbInboxEntityAttribute_page extends StatefulWidget {
 class _gbInboxEntityAttributeState extends State<gbInboxEntityAttribute_page> {
 
   gbEntityAttributeProvider _gbEntityAttributeProvider = new gbEntityAttributeProvider() ;
+  gbEntityProvider _gbEntityProvider = new gbEntityProvider();
   List<gb_tentity_attribute> lst_gb_tentity_attribute_provider = [];
   List<gb_tentity_attribute> lst_gb_tentity_attribute = [];
+  String S3getSignedUrl = "";
 
   // Declaración del Idioma.
   gbLanguage gb_lang = gbLanguage();
@@ -62,6 +67,11 @@ class _gbInboxEntityAttributeState extends State<gbInboxEntityAttribute_page> {
     });
   }
 
+  Future<void> get_gbEntityS3SignedUrl(String lv_tentity_id) async
+  {
+    S3getSignedUrl =  await _gbEntityProvider.getEntityS3getSignedUrl(widget.tentity_type, lv_tentity_id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,6 +81,58 @@ class _gbInboxEntityAttributeState extends State<gbInboxEntityAttribute_page> {
         floatingActionButton: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+              widget.tentity_type != 'DOC' ?
+              FloatingActionButton(
+                backgroundColor:  Color(0xFFf2f2f7),
+                foregroundColor: Colors.red,//Colors.indigo,
+                child: Icon(
+                    Icons.picture_as_pdf_rounded
+                ),
+                onPressed: () {
+                  var lv_tentity_id = widget.tentity_id.substring(widget.tentity_id.indexOf('-') + 1);
+                  get_gbEntityS3SignedUrl(lv_tentity_id).then((_) {
+
+                    Navigator.of(context).push
+                      (MaterialPageRoute(builder: (context) =>
+
+                        gbEntityPdfView_page(
+                            title: widget.tentity_label,
+                            url: S3getSignedUrl)
+                    ));
+
+                  });
+
+                },
+                heroTag: null,
+              )
+              :
+              FloatingActionButton(
+                backgroundColor:  Color(0xFFf2f2f7),
+                foregroundColor: Colors.indigo,
+                child: Icon(
+                    Icons.attach_file
+                ),
+                onPressed: () {
+                  var lv_tentity_id = widget.tentity_id.substring(widget.tentity_id.indexOf('-') + 1);
+                  get_gbEntityS3SignedUrl(lv_tentity_id).then((_) {
+
+                    Navigator.of(context).push
+                      (MaterialPageRoute(builder: (context) =>
+
+                        gbDocumentDownload_page(
+                            title: widget.tentity_label,
+                            tdocument_id: lv_tentity_id,
+                            url: S3getSignedUrl)
+                    ));
+
+                  });
+
+                },
+                heroTag: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
               FloatingActionButton(
                 backgroundColor: Colors.green,
                 foregroundColor: Colors.white,
